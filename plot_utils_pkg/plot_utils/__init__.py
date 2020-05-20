f46e442282fe93a46e1c195504623fa46a17346b
blhmc1 <bhendmore2@gmail.com>
2020-05-20 17:03:38 -0600

    update val_plot

--- plot_utils_pkg/plot_utils/__init__.py
+++ plot_utils_pkg/plot_utils/__init__.py
@@ -78,136 +78,107 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 '''
-@Param df1:
-     Dataframe of p-values for the first cancer (x-axis).
-     The first column must include the trans gene name.
-@Param df1_name:
-    String. Name of the x-axis. Include the first cancer name.
+@Param plot_df:
+     Dataframe with at least 2 columns of either p-value or correlation 
+     values for two cancers. Can include up to 3 additional pathway columns. 
+     Each pathway column represents a certain pathway or group of genes 
+     specified with boolean variables (True = is in the group). Each pathway  
+     column will become another scatterplot. 
+       NOTE: Column names will be used as titles for the pathway plots.
+     
+@Param val:
+    String. "corr": Correlation values
+            "pval": p-values
+    
+@Param x_axis_col:
+    String. Column label to plot on the x-axis.
 
-@Param df2:
-    Dataframe of p-values for the second cancer (y-axis).
-    The first column must include the trans gene name.
+@Param y_axis_col:
+    String. Column label to plot on the y-axis.
+    
+@Param plot_pathways:
+    List. Contains the column names of the pathway columns.
+    
+@Param hue_col:
+    String. Column label to base hue color.
 
-@Param df2_name:
-    String. Name of the y-axis. Include the second cancer name.
-
-@Param cat_df:
-    Dataframe of Booleans. First column must be gene names.
-    Can include up to 5 additional columns. Each column represents
-    a certain pathway or group of genes specified with boolean
-    variables (True is in the group). Each group will become another
-    plot graphing the p-values of the genes in that group.
+@Param hue_dict:
+    Dictionary. A dictionary with values from the hue_col as keys 
+    and chosen colors for the plot as values. 
 
 @Param save_file_name:
     String. Optional to save the figure. The name of the file to
     save the figure to.
 
-This function plots the p-values for two cancers where
-x = cancer1_pvals and y = cancer2_pvals.
+Returns:
+    Scatterplots.
 
-The wrap_ttest function will help with creating df1 and df2.
+The wrap_ttest function will help with creating plot_df.
 Both dataframes must have the gene included in the first column
 (omics can be included) and the p-value. Wrap_ttest creates this
 needed dataframe when its parameter return_all = True.
 
-The cat_df dataframe alows for multiple plots of specific gene groups
+The path_df dataframe alows for multiple plots of specific gene groups
 to be included. The first plot will always be all genes followed by one
-plot for each column included in cat_df. It is optional to include cat_df.
-The arrangement of the subplots follows in rows and columns: (1,2) for
-two plots, (2,2) for four plots(2,3) for six plots. The first plot will
-allways be the comprehensive p-values for all genes done in the t-test
-comparisons for the two cancers.
+plot for each column included in path_df. 
 '''
 
-def binary_pval_plot(df1, df1_name, df2, df2_name, cat_df=None, save_file_name=None):
-    # Step 1: Combine Dataframes
-    combined = df1.merge(df2, on=df1.columns[0]) # merge 2 pval df
-    combined = combined.replace(regex=True,to_replace='_proteomics', value='') # only gene names
-    combined_df = combined.rename(columns={combined.columns[1]: df1_name+" p-values",
-                                              combined.columns[2]: df2_name+" p-values"}) # Rename for use with x and y-axis
-    if cat_df is not None:
-        combined_df = combined_df.merge(cat_df, left_on= combined_df.columns[0], right_on= cat_df.columns[0]) # merge pathways
 
-    else: # Plots one plot if no cat_df provided
-        plt.figure(figsize=(6, 6))
-        all_pvals = sns.scatterplot(x=combined_df.columns[1], y=combined_df.columns[2], data=combined_df)
-        all_pvals.set_title("Comprehensive "+df1_name+" and "+df2_name+ " P-Values")
+def binary_val_plot(plot_df, val, x_axis_col, y_axis_col, title, pathway_cols = None, hue_col=None, 
+                    color_dict = None, save_file_name=None):
+    
+    f = plt.figure(figsize=(12, 12))
+    gs = f.add_gridspec(2, 2)
+    sns.set(font_scale = 1.2)
+        
+    # Create Main plot
+    all_pvals = f.add_subplot(gs[0, 0])
+    if hue_col is not None:
+        all_pvals = sns.scatterplot(x=x_axis_col, y=y_axis_col, data=plot_df,
+                                   hue=hue_col, palette=color_dict) 
+    else:
+        all_pvals = sns.scatterplot(x=x_axis_col, y=y_axis_col, data=plot_df)
+    all_pvals.set_title(title)    
+    plt.xlabel(x_axis_col)
+    plt.ylabel(y_axis_col)
+    
+    # Set scale of x and y axis
+    if val == 'corr':
+        axes = {'x_low': -1.5, 'x_high': 1.5, 'y_low': -1.5, 'y_high': 1.5}
+    elif val == 'pval':
         all_pvals.set_xscale('log')
         all_pvals.set_yscale('log')
-        plt.xlim(1e-5, 1e0) # 0.00005 to 1
-        plt.ylim(1e-5, 1e0)
-
-        if save_file_name is not None:
-            plt.savefig(save_file_name+'.png')
-        plt.show()
-        plt.clf()
-        plt.close()
-        return 0
-
-    # Step 2: Find number of plots needed (dimensions of array for subplots)
-    paths = list(cat_df.columns[1:])
-    total = len(paths) + 1
-    two_plots = False
-    if total == 2:
-        m_row = 1
-        m_col = 2
-        two_plots = True
-    elif total <= 4:
-        m_row = 2
-        m_col = 2
-    elif total <= 6:
-        m_row = 2
-        m_col = 3
-    else:
-        print("Two many columns in cat_df. Can only plot 5 pathways/groups. (6 total plots)")
-        return 1
-
-    fig, axes = plt.subplots(m_row, m_col, sharex=True, sharey=True) # share x -axis title
-    plt.rcParams['figure.figsize']=(15, 10) #size of plot
-    sns.set(font_scale = 1.1)
-
-    #Step 3: Create Multiple Plots
-    if two_plots == True:  ### needed because subplots dimensions are a 1D array, not 2D
-        # First plot with all p-values
-        ax = sns.scatterplot(x=combined_df.columns[1], y=combined_df.columns[2], data=combined_df,
-            ax=axes[0])
-        ax.set_title("Comprehensive "+df1_name+" and "+df2_name+ " P-Values")
-        ax.set_xscale('log')
-        ax.set_yscale('log')
-        plt.xlim(1e-5, 1e0) # 0.00005 to 1
-        plt.ylim(1e-5, 1e0)
-        # 1 Catagory plot
-        group_name = str(combined_df.columns[4])
-        only_p = combined_df.loc[combined_df[group_name] == True]
-        ax = sns.scatterplot(x=combined_df.columns[1], y=combined_df.columns[2], data=only_p, color='orange',
-            ax=axes[1]).set_title(group_name) #axes only has one number (1D array)
-
-    else:
-        # First plot with all p-values
-        ax = sns.scatterplot(x=combined_df.columns[1], y=combined_df.columns[2], data=combined_df,
-            ax=axes[0,0])
-        ax.set_title("Comprehensive "+df1_name+" and "+df2_name+ " P-Values")
-        ax.set_xscale('log')
-        ax.set_yscale('log')
-        plt.xlim(1e-5, 1e0) # 0.00005 to 1
-        plt.ylim(1e-5, 1e0)
-
-        # Catagory plots
+        axes = {'x_low': 1e-5, 'x_high': 1e0, 'y_low': 1e-5, 'y_high': 1e0}
+    plt.xlim(axes['x_low'], axes['x_high']) 
+    plt.ylim(axes['y_low'], axes['y_high'])
+    
+    
+    # Create subplots for pathways/groups in path_df
+    if pathway_cols is not None:
         i = 0
         j = 1
-        for e in paths:
-            only_p = combined_df.loc[combined_df[e] == True]
-            ax = sns.scatterplot(x=combined_df.columns[1], y=combined_df.columns[2], data=only_p, color='orange',
-                ax=axes[i, j]).set_title(e)
-            # i and j used to set next plot in axes
-            if i <= (m_row - 1) and j < (m_col - 1):
+        for col in pathway_cols:
+            bool_serries = plot_df[col]
+            group_df = plot_df[bool_serries] # keep genes in pathway
+            path = f.add_subplot(gs[i, j])
+            path = sns.scatterplot(x=x_axis_col, y=y_axis_col, data=group_df, color = 'orange')
+            if val == 'pval':
+                path.set_xscale('log')
+                path.set_yscale('log')
+            plt.xlim(axes['x_low'], axes['x_high']) 
+            plt.ylim(axes['y_low'], axes['y_high'])
+            path.set_title(col) 
+            plt.tight_layout()
+            
+            if i == j or i > j:
                 j += 1
-            elif j == (m_col - 1):
-                i+=1
-                j=0
-
+            elif i < j:
+                i += 1 
+                j -= 1
+        
     if save_file_name is not None:
         fig.savefig(save_file_name+'.png')
+        
     plt.show()
     plt.clf()
     plt.close()
