48e2039043a1d39dd0e0683b3e316d18c6d86dbd
Samuel Squires <sdsquires@pop-os.localdomain>
2022-10-02 12:52:27 -0600

    Everything should be working now

--- Pancan/refresh_from_TP53.sh
+++ Pancan/refresh_from_TP53.sh
@@ -1,4 +1,6 @@
+genes=(PTEN PIK3CA KRAS EGFR)
 for gene in ${genes[@]}; do 
+	echo "Replacing $gene"
 	rm -r $gene/*Effect_output;
 	rm $gene/full_analysis.ipynb;
 	cp TP53/full_analysis.ipynb $gene/full_analysis.ipynb;
